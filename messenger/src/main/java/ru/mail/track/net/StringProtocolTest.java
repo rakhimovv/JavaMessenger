4c25ec43f3f46c4a6409af0053202e62be8dedf5
rakhimovv <risonyo@gmail.com>
2015-11-17 13:11:50 +0300

    Added ojdbc6.jar

---
+++ messenger/src/main/java/ru/mail/track/net/StringProtocolTest.java
@@ -0,0 +1,29 @@
+package ru.mail.track.net;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by retina on 10/11/15.
+ */
+public class StringProtocolTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testDecode() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testEncode() throws Exception {
+
+    }
+}
